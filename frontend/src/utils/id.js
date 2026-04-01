20c80a42c6187dc7d2bd7e8229da352656f11946
DeepanshuAI <raijin4551@gmail.com>
2026-04-01 15:21:59 +0530

    Delete frontend directory

--- frontend/src/utils/id.js
+++
@@ -1,6 +0,0 @@
-export function createId(prefix = 'id') {
-  // Small, readable ID generator for localStorage records.
-  // Later we can use MongoDB ObjectIds from the backend.
-  return `${prefix}_${Date.now().toString(36)}_${Math.random().toString(36).slice(2, 8)}`
-}
-
